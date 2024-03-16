0f7b803ea77d6034c75779e8498219d41e207fcc
Michal Skop <michal.skop@kohovolit.eu>
2024-03-16 15:39:59 +0100

    other bets president

--- v0/update.py
+++ v0/update.py
@@ -7,39 +7,40 @@ import pandas as pd
 
 local_path = "v0/"
 
-event_id = 43804672
+event_ids = [43804672, 43804671]
 
-# load dataframe
-df = pd.read_csv(local_path + f'data/{event_id}.csv')
+for event_id in event_ids:
+  # load dataframe
+  df = pd.read_csv(local_path + f'data/{event_id}.csv')
 
-# download data
-# url = f"https://api.nike.sk/api/nikeone/v1/boxes/extended/sport-event-id?boxId=bi-137-4-26076&sportEventId={event_id}"
-# url = f"https://api.nike.sk/api/nikeone/v1/boxes/extended/sport-event-id?sportEventId={event_id}"
-# r = requests.get(url, verify=False)
-# downloaded_at = datetime.datetime.now().isoformat()
-# data = r.json()
+  # download data
+  # url = f"https://api.nike.sk/api/nikeone/v1/boxes/extended/sport-event-id?boxId=bi-137-4-26076&sportEventId={event_id}"
+  # url = f"https://api.nike.sk/api/nikeone/v1/boxes/extended/sport-event-id?sportEventId={event_id}"
+  # r = requests.get(url, verify=False)
+  # downloaded_at = datetime.datetime.now().isoformat()
+  # data = r.json()
 
-with open(local_path + f'source/data_{event_id}.json', 'r') as f:
-  data = json.load(f)
+  with open(local_path + f'source/data_{event_id}.json', 'r') as f:
+    data = json.load(f)
 
-read_at = datetime.datetime.now().isoformat()
+  read_at = datetime.datetime.now().isoformat()
 
-# parse data
-for bet in data['bets']:
-  b = {
-    # 'date': downloaded_at,
-    'date': read_at,
-    'header': bet['header'],
-    'name': bet['participantOrder'],
-    'odds_order': bet['oddsOrder'],
-  }
-  for row in bet['selectionGrid']:
-    for r in row:
-      if ('odds' in r.keys()):
-        bc = b.copy()
-        bc['odds_name'] = r['name']
-        bc['odds'] = r['odds']
-        bc['odds_enabled'] = r['enabled']
-        df = pd.concat([df, pd.DataFrame(bc, index=[0])])
+  # parse data
+  for bet in data['bets']:
+    b = {
+      # 'date': downloaded_at,
+      'date': read_at,
+      'header': bet['header'],
+      'name': bet['participantOrder'],
+      'odds_order': bet['oddsOrder'],
+    }
+    for row in bet['selectionGrid']:
+      for r in row:
+        if ('odds' in r.keys()):
+          bc = b.copy()
+          bc['odds_name'] = r['name']
+          bc['odds'] = r['odds']
+          bc['odds_enabled'] = r['enabled']
+          df = pd.concat([df, pd.DataFrame(bc, index=[0])])
 
-df.to_csv(local_path + f'data/{event_id}.csv', index=False)
+  df.to_csv(local_path + f'data/{event_id}.csv', index=False)
